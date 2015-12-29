c52d1f6969dd4f0a989b80f9337165fe484490a0
Tomasz Miąsko <tomasz.miasko@gmail.com>
2015-12-29 10:27:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # encoding: utf-8
 
 from distutils.core import setup, Extension
-from distutils.sysconfig import get_python_inc
 from numpy.distutils.misc_util import get_numpy_include_dirs
 import subprocess
 
@@ -14,13 +13,15 @@ def pkg_config(*args):
 
 setup(name='pyjags',
       version='0.1',
-      description='Python interface to JAGS',
+      description='Python interface to JAGS library for Bayesian data analysis.',
       author=u'Tomasz Miąsko',
+      author_email='tomasz.miasko@gmail.com',
       # TODO url
       license='GPL',
       classifiers=[
           'Development Status :: 3 - Alpha',
-          'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)'
+          'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+          'Operating System :: POSIX',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.4',
       ],
@@ -31,7 +32,7 @@ setup(name='pyjags',
               include_dirs=['pybind11/include/'] + get_numpy_include_dirs(),
               libraries=['jags'],
               define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
-              extra_compile_args=['-std=c++14'] + pkg_config('jags', '--cflags'),
+              extra_compile_args=['-std=c++11'] + pkg_config('jags', '--cflags'),
               extra_link_args=pkg_config('jags', '--libs'),
               sources=['pyjags/console.cc'])
       ],
