c0e1270f6d6d5a28ab68a9305c0f0ba22407dec5
Tomasz Miąsko <tomasz.miasko@gmail.com>
2015-12-31 13:44:41 +0100

    Remove special characters from source files.

--- pyjags/console.cc
+++ pyjags/console.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2015 Tomasz Miąsko
+// Copyright (C) 2015 Tomasz Miasko
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
